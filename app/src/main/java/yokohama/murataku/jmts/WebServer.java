a236956d476a21f602f6a2f2fe1b0da49b264dfa
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2022-08-21 14:08:54 +0900

    Add sample code to echo "Hello world"

---
+++ app/src/main/java/yokohama/murataku/jmts/WebServer.java
@@ -0,0 +1,9 @@
+package yokohama.murataku.jmts;
+
+import static spark.Spark.*;
+
+public class WebServer {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World!");
+    }
+}
